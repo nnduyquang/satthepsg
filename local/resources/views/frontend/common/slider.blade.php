6aba69ca7db037c7bda068696feb9f02ee133f7c
QUANG_CTY <nnduyquang@gmail.com>
2018-04-03 17:00:11 +0700

    ok

--- local/resources/views/frontend/common/slider.blade.php
+++ local/resources/views/frontend/common/slider.blade.php
@@ -1,9 +1,10 @@
 <div class="slider">
     <div class="slider-wrapper theme-default">
         <div id="slider" class="nivoSlider">
+            {{ Html::image('images/sliders/slider_4.JPG','',array('class'=>'slideshow')) }}
+            {{ Html::image('images/sliders/slider_5.png','',array('class'=>'slideshow')) }}
             {{ Html::image('images/sliders/slider_1.png','',array('class'=>'slideshow')) }}
             {{ Html::image('images/sliders/slider_2.png','',array('class'=>'slideshow')) }}
-            {{ Html::image('images/sliders/slider_3.png','',array('class'=>'slideshow')) }}
 
         </div>
     </div>
